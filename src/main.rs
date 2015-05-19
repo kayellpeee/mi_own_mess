952927eb1b3dcba72a7db568e6461e5b65aa1ccb
Keenan Lidral-Porter <keenan@surfair.com>
2015-05-18 17:54:51 -0700

    [feat] adds log crate with inital setup

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,31 @@
 extern crate mio;
+#[macro_use]
+extern crate log;
 
 use mio::*;
 use mio::tcp::{TcpListener, TcpStream};
+use log::{LogRecord, LogLevel, LogMetadata, SetLoggerError, LogLevelFilter};
+
+struct SimpleLogger;
+
+impl log::Log for SimpleLogger {
+    fn enabled (&self, metadata: &LogMetadata) -> bool {
+        metadata.level() <= LogLevel::Info
+    }
+
+    fn log(&self, record: &LogRecord) {
+        if self.enabled(record.metadata()) {
+            println!("{} - {}", record.level(), record.args());
+        }
+    }
+}
+
+pub fn init() -> Result<(), SetLoggerError> {
+    log::set_logger(|max_log_level| {
+        max_log_level.set(LogLevelFilter::Info);
+        Box::new(SimpleLogger)
+    })
+}
 
 fn main() {
 
@@ -9,9 +33,6 @@ fn main() {
     const CLIENT: Token = Token(1);
 
     let address: std::net::SocketAddr = "127.0.0.1:13265".parse().unwrap();
-
-    println!("here is address {:?}", address);
-
     let server = TcpListener::bind(&address).unwrap();
     let mut event_loop = EventLoop::new().unwrap();
     event_loop.register(&server, SERVER).unwrap();
